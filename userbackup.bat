5ac3bbadade5f936caa26987576c35bd408440bd
The Archangel <32396866+the4rchangel@users.noreply.github.com>
2018-07-13 16:08:21 -0700

    Add files via upload
    Backs up a Windows users Chrome settings / bookmarks, as well as the users Home directory.

---
+++ userbackup.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo This script will backup the user's Chrome settings and User Profile
+echo -------------------------------------------------------------------
+set /p drive=Enter USB Drive Letter:
+:: Backs up the Google Chrome Settings
+xcopy /s C:\Users\%USERNAME%\AppData\Local\Google %drive%:\
+:: Backs up the User Profile
+xcopy /s %UserProfile% %drive%:\
