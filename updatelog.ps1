d76ece49fa6a902fbc1d9019d38d221fad0d48df
The Archangel <32396866+the4rchangel@users.noreply.github.com>
2018-08-28 11:25:40 -0700

    Delete updatelog.ps1
    This is old and not as useless. Have built better windows log tools since then. Will upload those instead.

--- updatelog.ps1
+++
@@ -1,4 +0,0 @@
-# This script pulls all Windows Update logs with the string "failed" and outputs them to the desktop in a file called error.log
-
-Get-WindowsUpdateLog
-cat ~\Desktop\WindowsUpdate.log | Select-String failed >> ~\Desktop\error.log
